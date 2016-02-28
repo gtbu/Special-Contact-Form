8697216c3c72893b20281b5f35f7a199c6cad2e4
josh <oyejorge@gmail.com>
2016-02-28 12:38:47 -0700

    namespaced

--- contact_form.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-defined('is_running') or die('Not an entry point...');
-
-global $addonPathData;
-if (file_exists($addonPathData.'/contact_form.php'))
-	include($addonPathData.'/contact_form.php');
-
