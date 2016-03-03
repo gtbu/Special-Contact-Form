1b91531d582653e3d5dd768b026d249239c8c9e7
josh <oyejorge@gmail.com>
2016-03-03 08:54:55 -0700

    set ckDefault

--- Form.php
+++ Form.php
@@ -4,6 +4,8 @@ namespace Addon\SCF;
 
 defined('is_running') or die('Not an entry point...');
 
+define('ckDefault', "toolbar : \n[\n['Bold', 'Italic', 'Underline', '-', 'Undo', 'Redo', '-', 'NumberedList', 'BulletedList', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', '-',\n'Format', '-', 'Link', '-', 'About']\n],\ntoolbarLocation : 'bottom',\ntoolbarStartupExpanded:'true',\nuiColor : '#eeeeee',\nheight:'12em'\n");
+
 class Form{
 
 	protected $template;
